66b5e4ce181defc1a48df666de31e27bfe7a6349
David <david.fox0904@gmail.com>
2019-07-23 02:13:28 +0800

    add logo

--- src/views/EntryPage.vue
+++ src/views/EntryPage.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="entry-page">
+    <img alt="free cell logo" src="@img/logo.svg">
     <router-link :to="{ name: 'week-2:load-process' }">load</router-link>
   </div>
 </template>
