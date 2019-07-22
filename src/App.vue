7b698693f2697b68bdb1d5691c31166e6678e61d
David <david.fox0904@gmail.com>
2019-07-23 02:09:11 +0800

    stylesheets added

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,11 @@
 <template>
   <router-view id="app"/>
 </template>
+
+<style lang="less">
+@import './styles/reset.css';
+@import './styles/font-noto-tc.css';
+@import './styles/font-roboto.css';
+@import './styles/animate.min.css';
+@import './styles/global.less';
+</style>
