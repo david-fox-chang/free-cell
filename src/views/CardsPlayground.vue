157835f1889019bf49d561f1b3145eba8cd67075
David <david.fox0904@gmail.com>
2019-07-25 08:18:51 +0800

    draggable cells generator modfied

--- src/views/CardsPlayground.vue
+++ src/views/CardsPlayground.vue
@@ -1,128 +1,111 @@
 <template>
   <div class="cards-playground">
-    <draggable v-model="myArray1" group="people" @start="drag=true" @end="drag=false">
-      <div v-for="element in myArray1" :key="element">{{element}}</div>
-    </draggable>
-    <hr>
-    <draggable v-model="myArray2" group="people" @start="drag=true" @end="drag=false">
-      <div v-for="element in myArray2" :key="element">{{element}}</div>
-    </draggable>
-    <hr>
+    tableaux
 
-    <draggable v-model="myArray3" group="people" @start="drag=true" @end="drag=false">
-      <div v-for="element in myArray3" :key="element">{{element}}</div>
+    <draggable
+      v-for="i in 8"
+      :key="`cascade${i}`"
+      v-model="$data[`cascade${i}`]"
+      group="people"
+      @start="drag=true"
+      @end="drag=false"
+      @change="change"
+      tag="div"
+    >cascade {{ i }}
+      <span v-for="element in $data[`cascade${i}`]" :key="element">{{element}}</span>
+      <hr>
     </draggable>
-    <hr>
 
-    <draggable v-model="myArray4" group="people" @start="drag=true" @end="drag=false">
-      <div v-for="element in myArray4" :key="element">{{element}}</div>
+    <draggable
+      v-for="i in 4"
+      :key="`opencell${i}`"
+      v-model="$data[`opencell${i}`]"
+      group="people"
+      @start="drag=true"
+      @end="drag=false"
+      tag="div"
+    >opencell - {{ i }}
+    <span v-for="element in $data[`opencell${i}`]" :key="element">{{ element }}</span>
+    <hr>
     </draggable>
-    <hr>
 
-    <draggable v-model="myArray5" group="people" @start="drag=true" @end="drag=false">
-      <div v-for="element in myArray5" :key="element">{{element}}</div>
-    </draggable>
+    <draggable
+      v-for="i in 4"
+      :key="`foundation${i}`"
+      v-model="$data[`foundation${i}`]"
+      group="people"
+      @start="drag=true"
+      @end="drag=false"
+      tag="div"
+    >foundation - {{ i }}
+    <span v-for="element in $data[`foundation${i}`]" :key="element">{{ i }} - {{ element }}</span>
     <hr>
-
-    <draggable v-model="myArray6" group="people" @start="drag=true" @end="drag=false">
-      <div v-for="element in myArray6" :key="element">{{element}}</div>
-    </draggable>
-    <hr>
-
-    <draggable v-model="myArray7" group="people" @start="drag=true" @end="drag=false">
-      <div v-for="element in myArray7" :key="element">{{element}}</div>
-    </draggable>
-    <hr>
-
-    <draggable v-model="myArray8" group="people" @start="drag=true" @end="drag=false">
-      <div v-for="element in myArray8" :key="element">{{element}}</div>
-    </draggable>
-    <hr>
-
-    <draggable v-model="oneElArr1" group="people" @start="drag=true" @end="drag=false">
-      <div v-for="element in oneElArr1" :key="element">{{element}}</div>
-    </draggable>
-    <hr>
-    <draggable v-model="oneElArr2" group="people" @start="drag=true" @end="drag=false">
-      <div v-for="element in oneElArr2" :key="element">{{element}}</div>
-
-    </draggable>
-    <hr>
-    <draggable v-model="oneElArr3" group="people" @start="drag=true" @end="drag=false">
-      <div v-for="element in oneElArr3" :key="element">{{element}}</div>
-
-    </draggable>
-    <hr>
-    <draggable v-model="oneElArr4" group="people" @start="drag=true" @end="drag=false">
-      <div v-for="element in oneElArr4" :key="element">{{element}}</div>
-
-    </draggable>
-    <hr>
-    <draggable v-model="finPoker1" group="people" @start="drag=true" @end="drag=false">
-      <div v-for="element in finPoker1" :key="element">{{element}}</div>
-
-    </draggable>
-    <hr>
-    <draggable v-model="finPoker2" group="people" @start="drag=true" @end="drag=false">
-      <div v-for="element in finPoker2" :key="element">{{element}}</div>
-
-    </draggable>
-    <hr>
-    <draggable v-model="finPoker3" group="people" @start="drag=true" @end="drag=false">
-      <div v-for="element in finPoker3" :key="element">{{element}}</div>
-
-    </draggable>
-    <hr>
-    <draggable v-model="finPoker4" group="people" @start="drag=true" @end="drag=false">
-      <div v-for="element in finPoker4" :key="element">{{element}}</div>
-
     </draggable>
   </div>
 </template>
 
 <script>
 import draggable from 'vuedraggable';
+import lodash from 'lodash';
+
+// import OpenCells from '../components/OpenCells.vue';
+
 
 export default {
   components: {
     draggable,
+    // 'open-cells': OpenCells,
   },
-  data: () => ({
-    myArray1: [],
-    myArray2: [],
-    myArray3: [],
-    myArray4: [],
-    myArray5: [],
-    myArray6: [],
-    myArray7: [],
-    myArray8: [],
-    oneElArr1: [],
-    oneElArr2: [],
-    oneElArr3: [],
-    oneElArr4: [],
-    finPoker1: [],
-    finPoker2: [],
-    finPoker3: [],
-    finPoker4: [],
-  }),
+  data() {
+    const $return = [
+      { name: 'cascade', num: 8 },
+      { name: 'opencell', num: 4 },
+      { name: 'foundation', num: 4 },
+    ].reduce((sum, { name, num }) => Object.assign(
+      sum,
+      ...Array.from(
+        Array(num),
+        (i, ind) => ({ [`${name}${ind + 1}`]: [] }),
+      ),
+    ), {});
+
+    console.log($return);
+    return { ...$return };
+  },
   created() {
-    const pokers = ['♠', '♣', '♥', '♦'].map(i => [
+    const pokers = lodash.shuffle('♠♣♥♦').map(i => [
       'A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'j', 'q', 'k',
     ].map(n => `${i}-${n}`)).flat();
-
-    console.log(pokers, this.myArray7.push(...pokers));
+    console.log(pokers, this.cascade7.push(...lodash.shuffle(pokers)));
   },
-  watch: {
-    oneElArr1(newlist) {
-      console.log('oneElArr1', newlist.length);
-      if (newlist.length > 1) {
-        console.log('fail');
-      }
+  methods: {
+    change(...e) {
+      console.table(...e);
     },
   },
+  watch: [
+    { name: 'cascade', num: 8 },
+    { name: 'opencell', num: 4 },
+    { name: 'foundation', num: 4 },
+  ].reduce((sum, { name, num }) => Object.assign(
+    sum,
+    ...Array.from(
+      Array(num),
+      (i, ind) => ({
+        [`${name}${ind + 1}`]: (newlist) => {
+          console.log(`${name}${ind + 1}`, newlist);
+          if (newlist.length > 1) {
+            console.log('fail', newlist.length);
+          }
+        },
+      }),
+    ),
+  ), {}),
 };
 </script>
 
 <style lang="less" scoped>
-
+div {
+  border: solid 1px #F00;
+}
 </style>
